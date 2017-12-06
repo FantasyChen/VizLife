a195fa843801c3e8f2a520b1d75614201ad587ca
ShirleyXie <shirleyxie@ShirleyXies-MacBook-Pro.local>
2017-12-06 15:58:36 -0800

    draw charts dynamically from json obj

--- VizLife-mobile/www/js/dashboard1.js
+++ VizLife-mobile/www/js/dashboard1.js
@@ -1,10 +1,19 @@
 var dashboard1 = (function () {
 
     "use strict";
+    /* hardcode, assuming there are at most 10 metrics selected*/
     var chart0,
-        chart1;
+        chart1,
+        chart2,
+        chart3,
+        chart4,
+        chart5,
+        chart6,
+        chart7,
+        chart8,
+        chart9;
 
-    /* json files */
+    /* json files/obj goes here */
     var activity = {
                      "working out": {
                        "running": 200,
@@ -20,9 +29,15 @@ var dashboard1 = (function () {
                         "beef": 300,
                         "chicken": 500,
                         "sea food": 430
-                     }
+                     },
+                     "whatever": {
+                         "showing": 10,
+                         "drink": 20
+                      }
                    };
-    /* colors */
+    var chartNums = Object.keys(activity).length;
+
+    /* colors:  hardcode, assuming there are at most 10 colors/labels in a chart*/
     var colorSet = ['rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
@@ -31,10 +46,15 @@ var dashboard1 = (function () {
                     'rgba(255, 159, 64, 1)',
                     'rgba(103,80,85, 1)',
                     'rgba(59,144,107,1)',
-                    'rgba(206,187,110,1)'
+                    'rgba(206,187,110,1)',
+                    'rgba(161,159,151)'
                     ];
+
     /* create metricIdx-th chart*/
     function createChart(metricIdx) {
+       if(metricIdx >= chartNums) {
+            return null;
+       }
        var ctx = document.getElementById("myChart" + metricIdx.toString());
        var metricName = Object.keys(activity)[metricIdx];//string
        var curMetric = Object.values(activity)[metricIdx];
@@ -69,8 +89,7 @@ var dashboard1 = (function () {
         var html = '';
         var chartIdx = 0;
         var chartName;
-        var x;
-        for(x in activity) {
+        for(chartIdx = 0; chartIdx < chartNums; chartIdx++) {
             chartName = Object.keys(activity)[chartIdx];
             html = html +
                               '<div id="chart' +
@@ -83,7 +102,6 @@ var dashboard1 = (function () {
                               '<canvas id="myChart' +
                               chartIdx.toString() +
                               '" width="300" height="300"></canvas>';
-            chartIdx = chartIdx + 1;
         }
         return html;
     }
@@ -93,10 +111,17 @@ var dashboard1 = (function () {
         var html = generateHtml();
         $("#content").html(html);
 
-
         /* end loop for charts*/
         chart0 = createChart(0);
         chart1 = createChart(1);
+        chart2 = createChart(2);
+        chart3 = createChart(3);
+        chart4 = createChart(4);
+        chart5 = createChart(5);
+        chart6 = createChart(6);
+        chart7 = createChart(7);
+        chart8 = createChart(8);
+        chart9 = createChart(9);
     }
 
     return {
